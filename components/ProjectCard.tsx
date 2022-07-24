af0be4e05e749cc1035802723346c942e364fffb
Djalalov <edjalalov@outlook.com>
2022-07-24 21:14:00 +0500

    projects

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,9 +3,20 @@ import Image from "next/image";
 
 const ProjectCard = ({ image }) => {
 	return (
-		<div className="flex rounded-xl h-18 px-2 py-2 mx-4 my-2 bg-[#292C35] dark:bg-secondary/5">
-			<Image src={image} alt="Project 1" width={500} height={500} />
-		</div>
+		<>
+			<div className="card card-compact flex rounded-xl h-18 mx-4 my-2 bg-[#292C35] dark:bg-secondary/5 shadow-xl">
+				<figure>
+					<Image src={image} alt="Project 1" width={500} height={500} />{" "}
+				</figure>
+				<div className="card-body">
+					<h2 className="card-title">Shoes!</h2>
+					<p>If a dog chews shoes whose shoes does he choose?</p>
+					<div className="card-actions justify-end">
+						<button className="btn btn-primary">Buy Now</button>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import type { NextPage } from "next";
 import ProjectCard from "./ProjectCard";
 import project0 from "../public/project0.jpg";
+import project1 from "../public/project1.jpg";
+import project2 from "../public/project2.jpg";
+import project3 from "../public/project3.jpg";
 
 const Projects: NextPage = () => {
 	return (
@@ -12,27 +15,9 @@ const Projects: NextPage = () => {
 			</h1>
 
 			<ProjectCard image={project0} />
-
-			<p className="title">Loyihalar</p>
-			<h1 className="text-4xl font-bold px-4 dark:text-zinc-300">
-				Loremum ip lor sitwe amet elit
-			</h1>
-			<p className="title">Loyihalar</p>
-			<h1 className="text-4xl font-bold px-4 dark:text-zinc-300">
-				Loremum ip lor sitwe amet elit
-			</h1>
-			<p className="title">Loyihalar</p>
-			<h1 className="text-4xl font-bold px-4 dark:text-zinc-300">
-				Loremum ip lor sitwe amet elit
-			</h1>
-			<p className="title">Loyihalar</p>
-			<h1 className="text-4xl font-bold px-4 dark:text-zinc-300">
-				Loremum ip lor sitwe amet elit
-			</h1>
-			<p className="title">Loyihalar</p>
-			<h1 className="text-4xl font-bold px-4 dark:text-zinc-300">
-				Loremum ip lor sitwe amet elit
-			</h1>
+			<ProjectCard image={project1} />
+			<ProjectCard image={project2} />
+			<ProjectCard image={project3} />
 		</div>
 	);
 };
